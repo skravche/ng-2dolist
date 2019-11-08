ec0a120ee44310a7b1918c32b320e98f0298fd19
skravche <sktechno92@gmail.com>
2019-11-08 14:51:07 +0200

    added todo list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TodoListHeaderComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
